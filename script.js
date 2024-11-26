f6134e1f2bd4af570515e796aa4e7dec0a6b06dc
MaxAve <maxave909@gmail.com>
2024-11-26 15:49:40 +0100

    Fixed some stuff

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let draggedObjectMouseDiff = {x: 0, y: 0}
 
 let downloadButton = document.querySelector("#download-button")
 let renameButton = document.querySelector("#rename-button")
+let uploadButton = document.querySelector("#file-upload")
 
 const blockSize = {x: cellSize * 5, y: cellSize * 7}
 const switchRadius = cellSize
@@ -254,25 +255,22 @@ canvas.addEventListener('mousemove', (event) => {
 		dragDiff.y = 0
 		preDragMousePos.x = mouseX
 		preDragMousePos.y = mouseY
-		console.log(`${sketchOffset.x}, ${sketchOffset.y}`)
 	}
 });
 
-/*
-TODO account for sketch offsets
-*/
-
 canvas.addEventListener('mousedown', (event) => {
+	relX = mouseX - sketchOffset.x
+	relY = mouseY - sketchOffset.y
 	switch (event.button) {
 		case 0:
 			draggingObject = false
 			for(let [key, obj] of objectMap) {
-				if(obj.type == 1 && mouseX >= obj.position.x && mouseY >= obj.position.y && mouseX <= (obj.position.x + blockSize.x) && mouseY <= (obj.position.y + blockSize.y)) {
+				if(obj.type == 1 && relX >= obj.position.x && relY >= obj.position.y && relX <= (obj.position.x + blockSize.x) && relY <= (obj.position.y + blockSize.y)) {
 					draggedObjectID = key
 					draggedObjectMouseDiff.x = obj.position.x - mouseX
 					draggedObjectMouseDiff.y = obj.position.y - mouseY
 					draggingObject = true
-				} else if(obj.type == 2 && Math.sqrt(Math.pow(obj.position.x - mouseX, 2) + Math.pow(obj.position.y - mouseY, 2)) <= switchRadius) {
+				} else if(obj.type == 2 && Math.sqrt(Math.pow(obj.position.x - (mouseX - sketchOffset.x), 2) + Math.pow(obj.position.y - (mouseY - sketchOffset.y), 2)) <= switchRadius) {
 					draggedObjectID = key
 					draggedObjectMouseDiff.x = obj.position.x - mouseX
 					draggedObjectMouseDiff.y = obj.position.y - mouseY
@@ -287,7 +285,7 @@ canvas.addEventListener('mousedown', (event) => {
 			break
 		case 2:
 			for(let [key, obj] of objectMap) {
-				if(obj.type == 2 && Math.sqrt(Math.pow(obj.position.x - mouseX, 2) + Math.pow(obj.position.y - mouseY, 2)) <= switchRadius) {
+				if(obj.type == 2 && Math.sqrt(Math.pow(obj.position.x - (mouseX - sketchOffset.x), 2) + Math.pow(obj.position.y - (mouseY - sketchOffset.y), 2)) <= switchRadius) {
 					obj.powered = !obj.powered
 				}
 			}
@@ -324,6 +322,10 @@ downloadButton.addEventListener("click", () => {
     anele.click();
 })
 
+uploadButton.addEventListener("click", () => {
+	confirm('Sind Sie sicher, dass Sie eine neue Datei hochladen möchten? Die jetzige Skizze wird dann gelöscht!')
+})
+
 /*
  * GUI
 */
@@ -344,7 +346,7 @@ function drawWire(x1, y1, x2, y2) {
 
 function drawObject(id) {
 	const obj = objectMap.get(id)
-	ctx.lineWidth = 3
+	ctx.lineWidth = 2
 	ctx.strokeStyle = 'black'
 	const studLen = 20
 	switch(obj.type) {
@@ -426,7 +428,7 @@ function drawObject(id) {
 
 //createObject('block', 69, 'NAND');
 //objectMap.get(69).operation = NAND 
-const id69 = addLogicGateBlock(GateType.NAND)
+/*const id69 = addLogicGateBlock(GateType.NAND)
 createObject('block', 70, 'XOR');
 createObject('block', 71, 'XOR');
 objectMap.get(70).operation = XOR
@@ -434,12 +436,12 @@ objectMap.get(71).operation = XOR
 objectMap.get(70).input.push(false)
 connectWire(id69, 70, 4)
 connectWire(id69, 71,99)
-objectMap.get(99).valIndex = 0
+objectMap.get(99).valIndex = 0*/
 //connectWire(69, 70, 5)
 //objectMap.get(5).inIndex = 1
-objectMap.get(4).valIndex = 0
+//objectMap.get(4).valIndex = 0
 //objectMap.get(5).valIndex = 0
-objectMap.get(id69).output.pop()
+//objectMap.get(id69).output.pop()
 /*createObject('block', 79, 'XOR');
 connectWire(69, 79)
 connectWire(69, 79)
@@ -449,11 +451,11 @@ objectMap.get(420).input.push(false)
 connectWire(69, 420, 15)
 objectMap.get(15).inIndex = 1
 connectWire(79, 420)*/
-createObject('switch', 2)
-createObject('switch', 3)
-connectWire(2, id69)
-connectWire(3, id69, 23)
-objectMap.get(23).inIndex = 1
+//createObject('switch', 2)
+//createObject('switch', 3)
+//connectWire(2, id69)
+//connectWire(3, id69, 23)
+//objectMap.get(23).inIndex = 1
 
 function draw() {
 	if(draggedObjectID != null) {
@@ -468,14 +470,14 @@ function draw() {
 	ctx.lineWidth = 1
 	for(let i = 0; i <= canvas.width; i += cellSize) {
 		ctx.beginPath()
-		ctx.moveTo(i + (sketchOffset.x % cellSize), (sketchOffset.y % cellSize))
-		ctx.lineTo(i + (sketchOffset.x % cellSize), canvas.height + (sketchOffset.y % cellSize))
+		ctx.moveTo(i + (sketchOffset.x % cellSize), (sketchOffset.y % cellSize) - 100)
+		ctx.lineTo(i + (sketchOffset.x % cellSize), canvas.height + (sketchOffset.y % cellSize) + 100)
 		ctx.stroke()
 	}
 	for(let i = 0; i <= canvas.height; i += cellSize) {
 		ctx.beginPath()
-		ctx.moveTo(sketchOffset.x % cellSize, i + (sketchOffset.y % cellSize))
-		ctx.lineTo(canvas.width + (sketchOffset.x % cellSize), i + (sketchOffset.y % cellSize))
+		ctx.moveTo(sketchOffset.x % cellSize - 100, i + (sketchOffset.y % cellSize))
+		ctx.lineTo(canvas.width + (sketchOffset.x % cellSize) + 100, i + (sketchOffset.y % cellSize))
 		ctx.stroke()
 	}
 
